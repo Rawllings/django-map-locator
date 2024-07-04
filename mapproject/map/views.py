679210ec35f82322fe6146280f3df764d8fc5b02
Rawllings Kiprop <rutojr01@gmail.com>
2024-07-04 14:19:27 +0300

    Created a search database and superadmin page. I have connect with the UI using the form template and now you can search for any place

--- mapproject/map/views.py
+++ mapproject/map/views.py
@@ -1,16 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Search
 import folium
+import geocoder
+from .forms import SearchForm
 
 # Create your views here.
 
 def index(request):
+    if request.method == 'POST':
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    else:
+        form = SearchForm()
+    address = Search.objects.all().last()
+    location = geocoder.osm(address)
+    lat = location.lat
+    lng = location.lng
+    country = location.country
     # Create Map Object
     m = folium.Map(location=[19, -12], zoom_start=2)
-    folium.Marker([-1.2497, 36.9451], tooltip='Click for more', popup="Chokaa").add_to(m)
+    # folium.Marker([-1.2497, 36.9451], tooltip='Click for more', 
+    #               popup="Chokaa").add_to(m)
+    
+    folium.Marker([lat, lng], tooltip='Click for more', 
+                  popup=country).add_to(m)
+    
+
     # Get HTML Representation of Map    
     m = m._repr_html_()
     context = {
         'm': m,
+        'form': form
     }
     return render(request, 'index.html', context)
 
