3a6e66a8af5a86ae109fcfb79876dd7dfa1450f6
Rawllings Kiprop <rutojr01@gmail.com>
2024-07-04 15:28:57 +0300

    Done with the app

--- mapproject/map/views.py
+++ mapproject/map/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from .models import Search
+from .forms import SearchForm
+
 import folium
 import geocoder
-from .forms import SearchForm
 
 # Create your views here.
 
@@ -14,11 +16,18 @@ def index(request):
             return redirect('/')
     else:
         form = SearchForm()
+
     address = Search.objects.all().last()
     location = geocoder.osm(address)
     lat = location.lat
     lng = location.lng
     country = location.country
+
+    if lat == None and lng == None:
+        address.delete()
+        return HttpResponse('Invalid Location')
+
+
     # Create Map Object
     m = folium.Map(location=[19, -12], zoom_start=2)
     # folium.Marker([-1.2497, 36.9451], tooltip='Click for more', 
@@ -32,7 +41,7 @@ def index(request):
     m = m._repr_html_()
     context = {
         'm': m,
-        'form': form
+        'form': form,
     }
     return render(request, 'index.html', context)
 
