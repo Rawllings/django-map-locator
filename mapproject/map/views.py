e64db731887ca2f440fa1a2fcdb23084035261f7
Rawllings Kiprop <rutojr01@gmail.com>
2024-07-04 11:48:53 +0300

    Installed another package called GeoCoder for coordinates

--- mapproject/map/views.py
+++ mapproject/map/views.py
@@ -5,9 +5,15 @@ import folium
 
 def index(request):
     # Create Map Object
-    m = folium.Map()
+    m = folium.Map(location=[19, -12], zoom_start=2)
+    folium.Marker([-1.2497, 36.9451], tooltip='Click for more', popup="Chokaa").add_to(m)
+    # Get HTML Representation of Map    
     m = m._repr_html_()
     context = {
         'm': m,
     }
     return render(request, 'index.html', context)
+
+
+# Latitude: -1.286389
+# Longitude: 36.817223
