485f1e48006b6c2b0e837bf8e19d20c30a8876d3
Rawllings Kiprop <rutojr01@gmail.com>
2024-07-04 11:35:07 +0300

    Managed to add a map in my web page. It looks really nice

--- mapproject/map/views.py
+++ mapproject/map/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+import folium
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    # Create Map Object
+    m = folium.Map()
+    m = m._repr_html_()
+    context = {
+        'm': m,
+    }
+    return render(request, 'index.html', context)
